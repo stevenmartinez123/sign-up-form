a35d5c91d7098ce3441c90e35e677715cd6b449e
Steven Martinez <stevenmartinez1494@gmail.com>
2022-05-02 12:46:09 -0700

    read me file updated for project description

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # sign-up-form
+This project is to demonstrate the practice and use of forms to create a simple sign up page. 
+The sign up page will have requirements for all sign up fields. 
+javascript added to verify passwords match and if not the user will be notified and the form fields will highlight red. 
--- js/js.js
+++ js/js.js
@@ -2,7 +2,6 @@ const password = document.getElementById('password1');
 const confirmPassword = document.getElementById('confirmPassword1');
 const button = document.querySelector('#btn');
 const message = document.getElementById('message');
-const email = document.getElementById('email1');
 button.addEventListener('click', syntaxCheck);
 
 
@@ -10,7 +9,7 @@ function syntaxCheck() {
 
     //check if passwords are correct
     if (password.value != confirmPassword.value) {
-        message.innerText = "* passwords do not match";
+        message.innerText = "* Passwords do not match";
         message.style.fontSize="small";
         message.style.color="red";
         password.style.border="1px solid red";
